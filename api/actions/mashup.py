b59bc99a7576f469344a308fa1480c7a4ea56e81
Argen7um <argen7um.music@yandex.ru>
2024-02-12 21:21:32 +0700

    fix session.begin

--- api/actions/mashup.py
+++ api/actions/mashup.py
@@ -18,85 +18,79 @@ from api.schemas.mashup import DeleteMahsupResponse, DeleteMashupRequest
 async def _create_mashup(
     body: CreateMashupRequest, session: AsyncSession
 ) -> ShowMashup:
-    with session.begin():
-        user_id = body.user_id
-        user_dal = UserDAL(session)
-        user = await user_dal.is_exists(user_id)
+    user_id = body.user_id
+    user_dal = UserDAL(session)
+    user = await user_dal.is_exists(user_id)
 
-        audio = body.audio
+    audio = body.audio
 
-        if not user:
-            ...  # обработка того что пользователя нет в базе
+    if not user:
+        ...  # обработка того что пользователя нет в базе
 
-        getted_sources_id = body.sources
-        source_dal = SourceDAL(session)
-        sources = []
-        for source_id in getted_sources_id:
-            source = await source_dal.is_exists(source_id)
-            if source:
-                sources.append(source)
-            else:
-                ...  # оработка того что сурс отсутсвует в базе
+    getted_sources_id = body.sources
+    source_dal = SourceDAL(session)
+    sources = []
+    for source_id in getted_sources_id:
+        source = await source_dal.is_exists(source_id)
+        if source:
+            sources.append(source)
+        else:
+            ...  # оработка того что сурс отсутсвует в базе
 
-        if sources == []:
-            ...  # Обработка того что сурсов нет в базе
+    if sources == []:
+        ...  # Обработка того что сурсов нет в базе
 
-        mashup_dal = MashupDAL(session)
-        created_mashup = await mashup_dal.create_mashup(
-            name=body.name, audio=audio, user_id=user_id, sources=sources
-        )
+    mashup_dal = MashupDAL(session)
+    created_mashup = await mashup_dal.create_mashup(
+        name=body.name, audio=audio, user_id=user_id, sources=sources
+    )
 
-        user_dal.update_user(
-            user_id=user.user_id, mashups=user.mashups + [created_mashup]
-        )
+    user_dal.update_user(user_id=user.user_id, mashups=user.mashups + [created_mashup])
 
-        return created_mashup.to_schema_with_rel()
+    return created_mashup.to_schema_with_rel()
 
 
 async def _get_mashup(
     body: GetMashupRequest, session: AsyncSession
 ) -> List[ShowMashup]:
-    with session.begin():
-        mashup_dal = MashupDAL(session)
+    mashup_dal = MashupDAL(session)
 
-        if mashup_id := body.get("mashup_id"):
-            getted_mashups = [(await mashup_dal.get_by_id(mashup_id))]
-        elif name := body.get("name"):
-            getted_mashups = [(mashup_dal.get_mashups_by_name(name))]
-        elif user_id := body.get("user_id"):
-            getted_mashups = mashup_dal.get_mashups_by_user_id(user_id)
-        elif source_id := body.get("source_id"):
-            getted_mashups = mashup_dal.get_mashups_by_source_id(source_id)
-        else:
-            raise HTTPException(
-                status_code=status.HTTP_400_BAD_REQUEST,
-                detail="Unknown fields in body data",
-            )
+    if mashup_id := body.get("mashup_id"):
+        getted_mashups = [(await mashup_dal.get_by_id(mashup_id))]
+    elif name := body.get("name"):
+        getted_mashups = [(mashup_dal.get_mashups_by_name(name))]
+    elif user_id := body.get("user_id"):
+        getted_mashups = mashup_dal.get_mashups_by_user_id(user_id)
+    elif source_id := body.get("source_id"):
+        getted_mashups = mashup_dal.get_mashups_by_source_id(source_id)
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Unknown fields in body data",
+        )
 
-        return [
-            ShowMashup(
-                mashup_id=mashup.mashup_id,
-                name=mashup.name,
-                user_id=mashup.user_id,
-                source=[source.source_id for source in mashup.sources],
-            )
-            for mashup in getted_mashups
-        ]
+    return [
+        ShowMashup(
+            mashup_id=mashup.mashup_id,
+            name=mashup.name,
+            user_id=mashup.user_id,
+            source=[source.source_id for source in mashup.sources],
+        )
+        for mashup in getted_mashups
+    ]
 
 
 async def _update_mashup(
     mashup_id: int, body: UpdateMashupRequest, session: AsyncSession
 ) -> UpdateMashupResponse:
-    with session.begin():
-        mashup_dal = MashupDAL(session)
-        mashup_id = await mashup_dal.update_mashup(mashup_id, **body)
+    mashup_dal = MashupDAL(session)
+    mashup_id = await mashup_dal.update_mashup(mashup_id, **body)
     return UpdateMashupResponse(updated_mashup_id=mashup_id)
 
 
 async def _delete_mashup(
     body: DeleteMashupRequest, session: AsyncSession
 ) -> DeleteMahsupResponse:
-    with session.begin():
-        mashup_dal = MashupDAL(session)
-        mashup_id = await mashup_dal.delete_mashup(body.mashup_id)
+    mashup_dal = MashupDAL(session)
+    mashup_id = await mashup_dal.delete_mashup(body.mashup_id)
     return DeleteMahsupResponse(deleted_mashup_id=mashup_id)
--- api/actions/user.py
+++ api/actions/user.py
@@ -16,45 +16,41 @@ from api.schemas.user import UpdatedUserResponse, DeleteUserResponse, DeleteUser
 
 
 async def _create_user(body: CreateUserRequest, session: AsyncSession) -> ShowUser:
-    async with session.begin():
-        user_dal = UserDAL(session)
-        user = await user_dal.create_user(
-            name=body.name,
-            username=body.username,
-            email=body.email,
-            is_active=True,
-            hashed_password=body.hashed_password,
-        )
+    user_dal = UserDAL(session)
+    user = await user_dal.create_user(
+        name=body.name,
+        username=body.username,
+        email=body.email,
+        is_active=True,
+        hashed_password=body.hashed_password,
+    )
     return user.to_schema_without_rel()
 
 
 async def _get_user_by_id(user_id: int, session: AsyncSession) -> User:
-    async with session.begin():
-        user_dal = UserDAL(session)
-        return await user_dal.get_user_by_id(
-            user_id=user_id,
-        )
+    user_dal = UserDAL(session)
+    return await user_dal.get_user_by_id(
+        user_id=user_id,
+    )
 
 
 async def _get_user_by_mashup_id(mashup_id: int, session: AsyncSession) -> User:
-    async with session.begin():
-        mashup_dal = MashupDAL(session)
-        mashup = await mashup_dal.get_mashup_by_id(mashup_id)
-        if mashup is None:
-            raise HTTPException(
-                status_code=status.HTTP_404_NOT_FOUND,
-                detail=f"Mashup with id {mashup_id} not found.",
-            )
-        user_dal = UserDAL(session)
-        return await user_dal.get_user_by_mashup_id(mashup_id=mashup_id)
+    mashup_dal = MashupDAL(session)
+    mashup = await mashup_dal.get_mashup_by_id(mashup_id)
+    if mashup is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Mashup with id {mashup_id} not found.",
+        )
+    user_dal = UserDAL(session)
+    return await user_dal.get_user_by_mashup_id(mashup_id=mashup_id)
 
 
 async def _get_user_by_email(email: str, session: AsyncSession) -> User:
-    async with session.begin():
-        user_dal = UserDAL(session)
-        return await user_dal.get_user_by_email(
-            email=email,
-        )
+    user_dal = UserDAL(session)
+    return await user_dal.get_user_by_email(
+        email=email,
+    )
 
 
 async def _get_user(body: GetUserRequest, session: AsyncSession) -> ShowUser:
@@ -87,9 +83,8 @@ async def _get_user(body: GetUserRequest, session: AsyncSession) -> ShowUser:
 async def _delete_user(
     body: DeleteUserRequest, session: AsyncSession
 ) -> DeleteUserResponse:
-    async with session.begin():
-        user_dal = UserDAL(session)
-        user_id = await user_dal.delete_user(body.user_id)
+    user_dal = UserDAL(session)
+    user_id = await user_dal.delete_user(body.user_id)
 
     return DeleteUserResponse(deleted_user_id=user_id)
 
@@ -97,7 +92,6 @@ async def _delete_user(
 async def _update_user(
     new_data: UpdateUserRequest, user_id: int, session: AsyncSession
 ) -> UpdatedUserResponse:
-    async with session.begin():
-        user_dal = UserDAL(session)
-        user_id = await user_dal.update_user(user_id, **new_data)
+    user_dal = UserDAL(session)
+    user_id = await user_dal.update_user(user_id, **new_data)
     return UpdatedUserResponse(updated_user_id=user_id)
