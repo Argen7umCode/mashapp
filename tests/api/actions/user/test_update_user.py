3f57e488be773e2dedb5e9769eafa0b332c6dfdc
Argen7um <argen7um.music@yandex.ru>
2024-02-16 00:41:35 +0700

    add test updating user with different fields

--- tests/api/actions/user/test_update_user.py
+++ tests/api/actions/user/test_update_user.py
@@ -14,6 +14,11 @@ from db.models import User
 from tests.db_funcs import insert_into_db
 
 
+updated_name = 'Pavel'
+updated_username = 'aphexfan'
+updated_email = "aphexfan@mail.com"
+
+
 @pytest.fixture(scope="module")
 def prepare_new_test_data() -> dict[str, Any]:
     return {
@@ -25,10 +30,16 @@ def prepare_new_test_data() -> dict[str, Any]:
         "is_active": True,
     }
 
-
+@pytest.mark.parametrize("new_data", (({'name':updated_name},
+                                       {'username':updated_username},
+                                       {'email':updated_email},
+                                       {'name':updated_name, 'email':updated_email},
+                                       {'name':updated_name, 'username':updated_username},
+                                       {'email':updated_email, 'username':updated_username},
+                                       {'name':updated_name, 'email':updated_email, 'username':updated_username})))
 @pytest.mark.asyncio
-async def test_update_name_user(
-    _get_test_db, prepare_valid_test_data, prepare_new_test_data
+async def test_succsses_update_user(
+    _get_test_db, prepare_valid_test_data, new_data
 ):
     name = prepare_valid_test_data["name"]
     username = prepare_valid_test_data["username"]
@@ -39,10 +50,8 @@ async def test_update_name_user(
         test_user = User(**prepare_valid_test_data)
         await insert_into_db(test_user, session)
 
-        new_name = prepare_new_test_data['name']
-        print(new_name)
         user_data = UpdateUserRequest(
-            name=new_name
+            **new_data
         )
         updated_data = await _update_user(user_data, 
                                           test_user.id, 
@@ -55,9 +64,22 @@ async def test_update_name_user(
         updated_user = (await session.execute(query)).scalar_one_or_none()
 
         assert updated_user.id == test_user.id
-        assert updated_user.name == new_name
-        assert updated_user.username == test_user.username
-        assert updated_user.email == test_user.email
+
+        if new_name:=new_data.get('name'):
+            assert updated_user.name == new_name 
+        else:
+            assert updated_user.name == test_user.name
+
+        if new_username:=new_data.get('username'):
+            assert updated_user.username == new_username 
+        else:
+            assert updated_user.username == test_user.username
+
+        if new_email:=new_data.get('email'):
+            assert updated_user.email == new_email 
+        else:
+            assert updated_user.email == test_user.email
+
         assert updated_user.hashed_password == test_user.hashed_password
         
         
